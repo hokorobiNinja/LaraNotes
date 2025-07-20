db909c42f782fec362c661431eaa8fe8f5d12dd6
hokorobiNinja <hokorobininja@gmail.com>
2025-07-20 17:51:34 +0900

    Add flash message output to layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,18 @@
 
     <hr>
 
+    @if (session('success'))
+    <div style="color: green">
+        {{ session('success') }}
+    </div>
+    @endif
+
+    @if (session('error'))
+        <div style="color: red">
+            {{ session('error') }}
+        </div>
+    @endif
+
     @yield('content')
 </body>
 </html>
