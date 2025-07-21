b3fdcbb4d6abfb25e5b3cf21b6b04b14696fcd4d
hokorobiNinja <hokorobininja@gmail.com>
2025-07-21 14:33:35 +0900

    Add ProfileController

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function show()
+    {
+        return __METHOD__;
+    }
+    
+    public function edit()
+    {
+        return __METHOD__;
+    }
+
+    public function update()
+    {
+        return __METHOD__;
+    }
+}
